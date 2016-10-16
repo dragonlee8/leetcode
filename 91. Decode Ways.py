ff8b839a01f78798322d5a894c27f4a2beaba483
mobilepricedev <leehuan2004@gmail.com>
2016-10-16 19:35:49 -0400

    Update 91. Decode Ways.py

--- 91. Decode Ways.py
+++ 91. Decode Ways.py
@@ -4,24 +4,24 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
+        
         if not s:
             return 0
-        
-        from sets import Set
-        allowlist = Set();
-        
-        for i in range(1, 27):
-            allowlist.add(str(i));
+            
         n = len(s)
         dp = [0] * 2
+        dp[1] = 1
         
         for i in range(n):
             a1 = dp[i%2]
             a2 = dp[(i+1)%2]
-            if i > 0 and s[i-1:i+1] in allowlist:
-                a2 += a2
+            count = 0
+            if int(s[i]) != 0:
+                count += a2
+            if i > 0 and (s[i-1] == '1' or (s[i-1] == '2' and s[i] in {'0', '1', '2', '3', '4', '5', '6'})):
+                count += a1
             
-            if s[i] in allowlist:
-                a2 += a1
+            dp[i%2] = count
         
-        return dp[(i+1)%2]
+        
+        return dp[(n-1)%2]
