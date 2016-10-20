83bc2c7269122483732c453e4ce698d6eb220315
mobilepricedev <leehuan2004@gmail.com>
2016-10-20 18:28:16 -0400

    Update 44. Wildcard Matching.py

--- 44. Wildcard Matching.py
+++ 44. Wildcard Matching.py
@@ -38,3 +38,39 @@ class Solution(object):
                 return False
                 
         return True
+
+    
+    
+    class Solution:
+    """
+    @param s: A string 
+    @param p: A string includes "?" and "*"
+    @return: A boolean
+    """
+    def isMatch(self, s, p):
+        # write your code here
+        if not s and not p:
+            return False
+        
+        n = len(s)
+        m = len(p)
+        
+        dp = [[False for x in range(m+1)] for x in range(n+1)]
+        
+        dp[0][0] = True
+        if n == 0 and p.count('*') == m:
+            return True
+    
+        
+        for i in range(1, n+1):
+            for j in range(1, m+1):
+                if p[j-1] == '?':
+                    dp[i][j] = dp[i-1][j-1]
+                elif p[j-1] == '*':
+                    dp[i][j] = dp[i-1][j-1] or dp[i-1][j] or dp[i][j-1]
+                else:
+                    if s[i-1] == p[j-1]:
+                        dp[i][j] = dp[i-1][j-1]
+                    
+        return dp[n][m]
+            
