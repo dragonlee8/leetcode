e18327a7d3c47da3e95b3c21dc45b77809d77373
mobilepricedev <leehuan2004@gmail.com>
2018-03-16 20:29:18 -0400

    Update 572. Subtree of Another Tree.cpp

--- 572. Subtree of Another Tree.cpp
+++ 572. Subtree of Another Tree.cpp
@@ -31,3 +31,35 @@ public:
         return helper(s, t, true);
     }
 };
+
+
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+
+class Solution {
+public:
+    void serialize(TreeNode* s, std::ostringstream& os) {
+        if (!s) {
+            os << ",#";
+            return;
+        }
+        os << "," << s->val;
+        serialize(s->left, os);
+        serialize(s->right, os);
+    }
+    
+    bool isSubtree(TreeNode* s, TreeNode* t) {
+        std::ostringstream os1, os2;
+        serialize(s, os1);
+        serialize(t, os2);
+        return os1.str().find(os2.str()) != string::npos;
+    }
+};
